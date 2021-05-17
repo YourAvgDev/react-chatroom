2ee65677536b8d449a122332cc108378b8642a14
YourAvgDev <hnandan75@gmail.com>
2021-05-17 21:52:41 +0530

    Firebase changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, { useState, useEffect } from "react";
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+
 import Channel from "./components/Channel";
 import Header from "./components/Header";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import firebase from "./firebase";
 
 import "./App.css";
-
-firebase.initializeApp({
-  apiKey: "AIzaSyDSv6qHzaRD0U1vkk6Osn0lNFSu6cTSUi4",
-  authDomain: "react-chatroom-c7ef7.firebaseapp.com",
-  projectId: "react-chatroom-c7ef7",
-  storageBucket: "react-chatroom-c7ef7.appspot.com",
-  messagingSenderId: "474147204325",
-  appId: "1:474147204325:web:70cbd153b03967c65d780c",
-});
 
 const db = firebase.firestore();
 const auth = firebase.auth();
 
 function App() {
   const [user, setUser] = useState(() => firebase.auth.currentUser);
-  // const [initializing, setInitializing] = useState(true);
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
@@ -32,8 +21,6 @@ function App() {
         setUser(null);
       }
     });
-    // if(initializing)
-    //   setInitializing(false);
 
     return unsubscribe;
   }, []);
@@ -48,22 +35,11 @@ function App() {
     }
   };
 
-  // const signOut = async() =>{
-  //   try{
-  //     await auth.signOut();
-  //   }
-  //   catch(err){
-  //     console.log(err.message);
-  //   }
-  // };
-
-  // if(initializing) return "Loading...";
   return (
     <div>
       <Header auth={auth} user={user} />
       {user ? (
         <div>
-          {/* <Button onClick={signOut}>Sign out</Button> */}
           <Channel user={user} db={db}></Channel>
         </div>
       ) : (
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDSv6qHzaRD0U1vkk6Osn0lNFSu6cTSUi4",
+  authDomain: "react-chatroom-c7ef7.firebaseapp.com",
+  projectId: "react-chatroom-c7ef7",
+  storageBucket: "react-chatroom-c7ef7.appspot.com",
+  messagingSenderId: "474147204325",
+  appId: "1:474147204325:web:70cbd153b03967c65d780c",
+});
+
+export default firebase;
